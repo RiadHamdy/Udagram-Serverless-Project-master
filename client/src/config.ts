2835bd2e35b4d22c87656ed169b6bd032027206c
RiadHamdy <riad.hamdy-riad@vodafone.com>
2021-12-14 08:09:12 +0200

    commit

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'wg306eouqb'
-export const apiEndpoint = `https://wg306eouqb.execute-api.us-east-2.amazonaws.com/dev`
+export const apiEndpoint = `https://$wg306eouqb.execute-api.us-east-2.amazonaws.com/dev`
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-iz8e20v1.us.auth0.com',            // Auth0 domain
