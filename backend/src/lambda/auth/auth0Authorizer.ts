77b59609b36ace481b9e6cac1f493a8a93b9f179
RiadHamdy <riad.hamdy-riad@vodafone.com>
2021-12-15 13:21:10 +0200

    commit

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -2,16 +2,22 @@ import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
-import Axios from 'axios'
 import { createLogger } from '../../utils/logger'
+import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-const jwksUrl = 'https://dev-iz8e20v1.us.auth0.com/.well-known/jwks.json'
 
-export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a userId', event.authorizationToken)
+// TODO: Provide a URL that can be used to download a certificate that can be used
+// to verify JWT token signature.
+// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
+const jwksUrl = 'https://dev-kmqb1zim.auth0.com/.well-known/jwks.json'
+
+export const handler = async (
+  event: CustomAuthorizerEvent
+): Promise<CustomAuthorizerResult> => {
+  logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
@@ -51,31 +57,19 @@ export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAutho
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
-  const kid = jwt.header.kid
 
-  const key = await getKey(kid)
-  const certificate = getCertificate(key)
+  // TODO: Implement token verification done
+  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
+  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
+  // return undefined
+  const { header } = jwt;
 
-  return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
+  let key = await getSigningKey(jwksUrl, header.kid)
+  return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
+
 }
 
-function getCertificate(key: string): string {
-  return `-----BEGIN CERTIFICATE-----\n${key}\n-----END CERTIFICATE-----`
-}
-
-async function getKey(kid: string) {
-  const reply = await Axios.get(jwksUrl)
-  const keys: any[] = reply.data.keys
-
-  const key = keys.find(key => key.kid === kid && key.kty === 'RSA')
-  if (!key) {
-    throw new Error('Key not found')
-  }
-
-  return key.x5c[0]
-}
-
-function getToken(authHeader: string): string {
+ function  getToken(authHeader: string): string {
   if (!authHeader) throw new Error('No authentication header')
 
   if (!authHeader.toLowerCase().startsWith('bearer '))
@@ -85,4 +79,38 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+const getSigningKey = async (jwkurl, kid) => {
+  let res = await Axios.get(jwkurl, {
+    headers: {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "'true'",
+    }
+  });
+  let keys  = res.data.keys;
+  // since the keys is an array its possible to have many keys in case of cycling.
+  const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
+      && key.kty === 'RSA' // We are only supporting RSA
+      && key.kid           // The `kid` must be present to be useful for later
+      && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
+    ).map(key => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+    });
+  const signingKey = signingKeys.find(key => key.kid === kid);
+  if(!signingKey){
+    throw new Error('Invalid signing keys')
+    logger.error("No signing keys found")
+  }
+
+  logger.info("Signing keys created successfully ", signingKey)
+  return signingKey
+
+};
+
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
 }
