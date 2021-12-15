b46f329d090badb7c0eb5a4d20aa0db78ee6ce43
RiadHamdy <riad.hamdy-riad@vodafone.com>
2021-12-15 09:00:20 +0200

    comiit

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -2,21 +2,18 @@ import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
+import { createLogger } from '../../utils/logger'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-
 const jwksUrl = 'https://dev-iz8e20v1.us.auth0.com/.well-known/jwks.json'
 
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+  logger.info('Authorizing a userId', event.authorizationToken)
   try {
-    const jwtToken = await  verifyToken(event.authorizationToken)
+    const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
 
     return {
@@ -33,7 +30,7 @@ export const handler = async (
       }
     }
   } catch (e) {
-    logger.error('User not authorized', { error: e })
+    logger.error('User not authorized', { error: e.message })
 
     return {
       principalId: 'user',
@@ -51,25 +48,31 @@ export const handler = async (
   }
 }
 
- async function verifyToken(authHeader: string): Promise<JwtPayload> {
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const kid = jwt.header.kid
 
-  if(!jwt){
-    throw new Error('invalid token')
+  const key = await getKey(kid)
+  const certificate = getCertificate(key)
+
+  return verify(token, certificate, { algorithms: ['RS256'] }) as JwtPayload
+}
+
+function getCertificate(key: string): string {
+  return `-----BEGIN CERTIFICATE-----\n${key}\n-----END CERTIFICATE-----`
+}
+
+async function getKey(kid: string) {
+  const reply = await Axios.get(jwksUrl)
+  const keys: any[] = reply.data.keys
+
+  const key = keys.find(key => key.kid === kid && key.kty === 'RSA')
+  if (!key) {
+    throw new Error('Key not found')
   }
 
-  try {
-    const response = await Axios.get(jwksUrl);
-    console.log(response);
-    var verifedToken = verify(token,response.data,{algorithms:['RS256']})
-
-    console.log('verfied toekn',verifedToken)
-    return  verifedToken as JwtPayload
-  } catch (error) {
-    console.error(error);
-    return undefined
-  }
+  return key.x5c[0]
 }
 
 function getToken(authHeader: string): string {
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'hrt1p95l25'
-export const apiEndpoint = 'https://hrt1p95l25.execute-api.us-east-2.amazonaws.com/dev'
+const apiId = 'qj0aohn64k'
+export const apiEndpoint = 'https://qj0aohn64k.execute-api.us-east-2.amazonaws.com/dev'
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-iz8e20v1.us.auth0.com',            // Auth0 domain
